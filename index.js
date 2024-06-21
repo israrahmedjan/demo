8a0cac9745fdaca896e3bd79ff18ba13e8f86172
israrahmedjan <israrahmedjan@gmail.com>
2024-06-21 11:40:04 +0500

    hello index file changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World file can be ABCD EFGH');
+  res.send('Hello World file Welcome to hello programme!');
 });
 
 app.listen(port, () => {
